d8fc6fe811e0f5619b38b85a29e1f4109ea88e5f
jennachuu <srchoo19@gmail.com>
2024-12-20 01:50:54 +0900

    changed the root path of src

--- src/pages/mypage/MyPage.tsx
+++ src/pages/mypage/MyPage.tsx
@@ -26,7 +26,7 @@ const MyPage: React.FC<MyPageProps> = ({
       <div className="flex flex-col md:flex-row items-center justify-center w-full max-w-2xl mt-16 relative ">
         <div className="flex-shirnk-0 w-40 h-40 rounded-full overflow-hidden md:absolute md:left-10 md:mx-20">
           <img
-            src={profileIcon}
+            src="/public/profileIcon.png"
             className="w-full h-full object-cover "
             alt="Profile Icon"
           />
