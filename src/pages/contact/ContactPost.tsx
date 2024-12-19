283ac4095bcb8f0af03d816f0d80e906ec7bbe7a
sujinnn <nmnm0919@naver.com>
2024-12-19 14:59:03 +0900

    #10 공방연결문의하기 반응형 UI

--- src/pages/contact/ContactPost.tsx
+++ src/pages/contact/ContactPost.tsx
@@ -2,17 +2,17 @@ const ContactPost = () => {
   return (
     <div
       className="flex flex-col justify-center items-center"
-      style={{ height: 'calc(100vh - 3rem' }}
+      //   style={{ height: 'calc(100vh - 3rem' }}
     >
-      <div className="flex w-[81rem]">
-        <h1 className="text-[#6E6E6E] text-[4.6rem] font-bold mr-[2.3rem] mb-[8.1rem]">
+      <div className="lg:flex lg:w-[81rem]">
+        <h1 className="text-[#6E6E6E] text-[4.6rem] font-bold mr-[2.3rem]">
           CONTACT
         </h1>
-        <p className="text-center pt-[2.8rem] text-[2rem] font-normal text-[#AEAEAE]">
+        <p className="text-center lg:pt-[2.8rem] text-[2rem] lg:mb-[8.1rem] md:mb-[5rem] font-normal text-[#AEAEAE]">
           공방 연결에 대한 문의를 남겨주시면 담당자가 확인 후 연락드리겠습니다.
         </p>
       </div>
-      <div className="flex">
+      <div className="lg:flex">
         <div className="flex flex-col">
           <p className="w-[38.5rem] text-[1.5rem] text-[#AEAEAE]">이름</p>
           <input
