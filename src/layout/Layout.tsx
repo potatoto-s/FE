dcb4b221e75adf9b2a94b4e791ebe8d849475f11
wave-rec <werther0704@gmail.com>
2024-12-23 19:13:45 +0900

    #12 fix/레이아웃 불필요한 아웃렛 코드 삭제

--- src/layout/Layout.tsx
+++ src/layout/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom';
 import Header from '../components/header/Header';
 import Footer from '../components/footer/Footer';
 
-Outlet;
 export default function Layout() {
   return (
     <div>
