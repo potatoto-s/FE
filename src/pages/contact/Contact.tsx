211802b1f14f275f13cc4de3538a17d4ac711e16
sujinnn <nmnm0919@naver.com>
2024-12-23 10:17:07 +0900

    #9 feat/UI 크기 수정

--- src/pages/contact/Contact.tsx
+++ src/pages/contact/Contact.tsx
@@ -15,28 +15,28 @@ const Contact = () => {
       className="flex flex-col justify-center items-center lg:mr-[40%] mt-[6rem]"
       style={{ height: 'calc(100vh - 28rem' }}
     >
-      <h1 className="w-[38rem] text-[#6E6E6E] text-[4.6rem] font-bold mb-[3.1rem]">
+      <h1 className="w-[32rem] text-[#6E6E6E] text-[4rem] font-bold mb-[3.1rem]">
         CONTACT
       </h1>
-      <div className="w-[38rem] h-[0.2rem] bg-[#6E6E6E]"></div>
-      <div className="flex justify-between w-[38rem]">
-        <p className="my-[2.5rem]  text-[2.5rem] font-bold text-[#F28749]">
+      <div className="w-[32rem] h-[0.2rem] bg-[#6E6E6E]"></div>
+      <div className="flex justify-between w-[32rem]">
+        <p className="my-[2.5rem] text-[2rem] font-bold text-[#F28749]">
           컨설팅 문의하기
         </p>
         <button onClick={() => handleNavigation('WORKSHOP')}>
           <GoArrowRight size={60} className="mr-[2rem]" />
         </button>
       </div>
-      <div className="w-[38rem] h-[0.2rem] bg-[#6E6E6E]"></div>
-      <div className="flex justify-between w-[38rem]">
-        <p className="my-[2.5rem]  text-[2.5rem] font-bold text-[#F28749]">
+      <div className="w-[32rem] h-[0.2rem] bg-[#6E6E6E]"></div>
+      <div className="flex justify-between w-[32rem]">
+        <p className="my-[2.5rem] text-[2rem] font-bold text-[#F28749]">
           공방 연결 문의하기
         </p>
         <button onClick={() => handleNavigation('COMPANY')}>
           <GoArrowRight size={60} className="mr-[2rem]" />
         </button>
       </div>
-      <div className="w-[38rem] h-[0.2rem] bg-[#6E6E6E]"></div>
+      <div className="w-[32rem] h-[0.2rem] bg-[#6E6E6E]"></div>
     </div>
   );
 };
