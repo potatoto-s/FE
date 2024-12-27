98bf7c7fe0fceceac40ed7afbe0bcb77d17588a8
wave-rec <werther0704@gmail.com>
2024-12-27 21:16:58 +0900

    #45 fix/accessToken, refreshToken 로컬스토리지에서 삭제 #45 fix/persist 데이터 삭제

--- src/stores/authStore.ts
+++ src/stores/authStore.ts
@@ -1,4 +1,5 @@
 import { create } from 'zustand';
+import { persist } from 'zustand/middleware';
 
 interface AuthState {
   accessToken: string | null;
@@ -6,13 +7,21 @@ interface AuthState {
   logout: () => void;
 }
 
-const useAuthStore = create<AuthState>((set) => ({
-  accessToken: null,
-  setAccessToken: (token) => set({ accessToken: token }),
-  logout: () => {
-    set({ accessToken: null });
-    localStorage.removeItem('refreshToken');
-  },
-}));
+const useAuthStore = create(
+  persist<AuthState>(
+    (set) => ({
+      accessToken: null,
+      setAccessToken: (token) => set({ accessToken: token }),
+      logout: () => {
+        set({ accessToken: null }); // Zustand 상태에서 accessToken 삭제
+        localStorage.removeItem('refreshToken'); // 로컬 스토리지에서 refreshToken 삭제
+        localStorage.removeItem('auth'); // Zustand persist 데이터 전체 삭제
+      },
+    }),
+    {
+      name: 'auth', // persist된 데이터의 키
+    }
+  )
+);
 
 export default useAuthStore;
