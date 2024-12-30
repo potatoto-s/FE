3fbcf5bb525db96a782921e7510590d5ea32c10e
wave-rec <werther0704@gmail.com>
2024-12-30 11:54:58 +0900

    #45 user 전체 초기화 로직 추가

--- src/stores/userStore.ts
+++ src/stores/userStore.ts
@@ -20,20 +20,7 @@ const useUserStore = create(
     (set) => ({
       user: null,
       setUser: (user) => set({ user }),
-      clearUser: () =>
-        set((state) => ({
-          user: state.user
-            ? {
-                email: state.user.email, // 이메일만 유지
-                name: '',
-                nickname: '',
-                phone: '',
-                role: '',
-                company_name: undefined,
-                workshop_name: undefined,
-              }
-            : null,
-        })),
+      clearUser: () => set({ user: null }), // user 전체 초기화
     }),
     {
       name: 'user', // 로컬스토리지 키
