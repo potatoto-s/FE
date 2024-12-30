6aef7e95d2673de13a29b0c04ddc5eef007d9678
wave-rec <werther0704@gmail.com>
2024-12-30 12:05:06 +0900

    #45 fix/실제 API 주소로 수정

--- src/api/authApi.tsx
+++ src/api/authApi.tsx
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 // 로그인 API
 export const loginApi = async (email: string, password: string) => {
-  const response = await axios.post('/api/login/', {
+  const response = await axios.post('https://hands.p-e.kr/login/', {
     email,
     password,
   });
