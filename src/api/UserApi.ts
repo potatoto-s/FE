652e2d15ebc085ce3bd31c2b65ad4caacbe76046
jennachuu <srchoo19@gmail.com>
2025-01-03 00:32:30 +0900

    created UserApi file and added api connection for mypage

---
+++ src/api/UserApi.ts
@@ -0,0 +1,11 @@
+import axiosAuthInstance from './axiosAuthInstance';
+
+export const fetchUserProfile = async () => {
+  try {
+    const reponse = await axiosAuthInstance.get('/api/profile/${id}');
+    return reponse.data;
+  } catch (error) {
+    console.log('사용자 프로필을 가져오는 중 오류 발생', error);
+    throw error;
+  }
+};
