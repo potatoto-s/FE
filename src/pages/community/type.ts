7701046d694c1cd274e301fe00e9468c13878d5c
kimhoseon <kmad910119@gmail.com>
2025-01-09 15:43:57 +0900

    Refactor: #110 타입정의 분리

---
+++ src/pages/community/type.ts
@@ -0,0 +1,13 @@
+export type FormType = 'post' | 'edit';
+
+export type FormData = {
+  category: string;
+  title: string;
+  content: string;
+  images: File[] | null; // image는 null 또는 File 타입
+};
+
+export type ErrorState = {
+  category: string | null;
+  image: string | null | undefined;
+};
