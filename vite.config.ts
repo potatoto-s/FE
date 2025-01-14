a099ad555910855525a9d328cec4c258f8d171c9
wave-rec <werther0704@gmail.com>
2025-01-14 20:23:38 +0900

    #121 vite-tsconfig-paths 설치

--- vite.config.ts
+++ vite.config.ts
@@ -1,18 +1,7 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [react()],
-  resolve: {
-    alias: [
-      { find: '@components', replacement: '/src/components' },
-      { find: '@pages', replacement: '/src/pages' },
-      { find: '@hooks', replacement: '/src/hooks' },
-      { find: '@layout', replacement: '/src/layout' },
-      { find: '@schemas', replacement: '/src/schemas' },
-      { find: '@stores', replacement: '/src/stores' },
-      { find: '@api', replacement: '/src/api' },
-      { find: '@', replacement: '/src' },
-    ],
-  },
+  plugins: [react(), tsconfigPaths()],
 });
