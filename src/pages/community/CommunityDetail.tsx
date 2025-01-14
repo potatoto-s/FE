364db1e1d0e5dfcc1d5449cfa686b0cdec5649d3
mac <chanhee0708jdm@gmail.com>
2025-01-14 17:34:45 +0900

    feat/ 댓글수 표시 반영

--- src/pages/community/CommunityDetail.tsx
+++ src/pages/community/CommunityDetail.tsx
@@ -13,6 +13,7 @@ import {
   deleteComment,
   toggleLikePost,
   deletePost,
+  // fetchComments,
 } from './CommunityDetailAPI';
 import useUserStore from '../../stores/userStore';
 import { Comment, Post } from './CommunityDetailTypes';
@@ -20,7 +21,10 @@ import { Comment, Post } from './CommunityDetailTypes';
 const CommunityDetail = () => {
   const { user } = useUserStore();
   const { id } = useParams<{ id: string }>();
+  const navigate = useNavigate();
+
   const [comments, setComments] = useState<Comment[]>([]);
+  const [commentCount, setcommentCount] = useState<number>(0);
   const [newComment, setNewComment] = useState<string>('');
   const [likes, setLikes] = useState<number>(0);
   const [isLiked, setIsLiked] = useState<boolean>(false);
@@ -32,7 +36,6 @@ const CommunityDetail = () => {
   const [showEditModal, setShowEditModal] = useState<boolean>(false);
   const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
   const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
-  const navigate = useNavigate();
 
   //게시글 세부 정보 로드
   useEffect(() => {
@@ -42,15 +45,19 @@ const CommunityDetail = () => {
           const postDetail = await fetchPostDetail(id);
           setPost(postDetail);
           setComments(postDetail.comments);
-          setLikes(postDetail.likes || 0);
+          setLikes(postDetail.like_count);
           setIsLiked(postDetail.is_liked);
+          setcommentCount(postDetail.comment_count);
+
+          // const comments = await fetchComments(id);
+          // setComments(comments);
         } catch (error) {
           console.error('게시글 로드 오류 발생', error);
         }
       }
     };
     loadPostDetails();
-  }, []); // 빈 배열로 설정
+  }, [id]); // 빈 배열로 설정
 
   const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
     setNewComment(e.target.value);
@@ -111,10 +118,13 @@ const CommunityDetail = () => {
         author: {
           id: user.id,
           role: user.role,
+          nickname: user.nickname,
           workshop_name: user.workshop_name,
         },
       };
       setComments([newCommentEntry, ...comments]);
+      setcommentCount((prevCount) => prevCount + 1);
+      setNewComment('');
       setShowModal(true);
       setTimeout(() => setShowModal(false), 2000);
     };
@@ -134,7 +144,10 @@ const CommunityDetail = () => {
   // 댓글 삭제 처리
   const handleCommentDelete = async (id: number) => {
     await deleteComment(id);
-    setComments(comments.filter((comment) => comment.id !== id));
+    setComments((prevComments) =>
+      prevComments.filter((comment) => comment.id !== id)
+    );
+    setcommentCount((prevCount) => prevCount - 1);
     setShowDeleteModal(true);
     setTimeout(() => setShowDeleteModal(false), 2000);
   };
@@ -160,7 +173,7 @@ const CommunityDetail = () => {
     } catch (error) {
       console.error('좋아요 상태 업데이트 오류 발생', error);
       setIsLiked(!newLikedStatus); // 오류시 되돌리기
-      setLikes((prevLikes) => prevLikes - (newLikedStatus ? 1 : -1)); // 원래 상태로 되돌리기
+      setLikes((prevLikes) => prevLikes - (newLikedStatus ? 1 : -1));
     }
   };
 
@@ -205,7 +218,7 @@ const CommunityDetail = () => {
                 </h1>
                 <h1 className="text-xl font-bold ml-2">{post.title}</h1>
                 <span className="text-gray-500 text-lg ml-2 flex items-center">
-                  <FaCommentDots className="mr-1" />({comments.length})
+                  <FaCommentDots className="mr-1" />({commentCount})
                 </span>
               </div>
               {user && post.author.id === user.id && (
@@ -241,7 +254,7 @@ const CommunityDetail = () => {
                     key={image.id}
                     src={image.image_url}
                     alt={`게시글 이미지 ${image.id}`}
-                    className="w-80 h-80 mb-4"
+                    className="max-w-full h-auto"
                   />
                 ))}
               </div>
@@ -331,3 +344,344 @@ const CommunityDetail = () => {
 };
 
 export default CommunityDetail;
+
+// import { useParams } from 'react-router-dom';
+// import { useEffect, useState } from 'react';
+// import { FaCommentDots } from 'react-icons/fa';
+// import { BsChatHeart } from 'react-icons/bs';
+// import { useNavigate } from 'react-router-dom';
+// import CommentList from '../../components/comment/CommentList';
+// import CommentInput from '../../components/comment/CommentInput';
+// import ConfirmModal from '../../components/modal/ConfirmModal';
+// import {
+//   fetchPostDetail,
+//   createComment,
+//   updateComment,
+//   deleteComment,
+//   toggleLikePost,
+//   deletePost,
+// } from './CommunityDetailAPI';
+// import useUserStore from '../../stores/userStore';
+// import { Comment, Post } from './CommunityDetailTypes';
+
+// const CommunityDetail = () => {
+//   const { user } = useUserStore();
+//   const { id } = useParams<{ id: string }>();
+//   const navigate = useNavigate();
+
+//   const [comments, setComments] = useState<Comment[]>([]);
+//   const [newComment, setNewComment] = useState<string>('');
+//   const [likes, setLikes] = useState<number>(0);
+//   const [isLiked, setIsLiked] = useState<boolean>(false);
+//   const [commentCount, setCommentCount] = useState<number>(0);
+//   const [post, setPost] = useState<Post | null>(null);
+//   const [showModal, setShowModal] = useState<boolean>(false);
+//   const [showConfirmModal, setShowConfirmModal] = useState<boolean>(false);
+//   const [showDeleteModal, setShowDeleteModal] = useState<boolean>(false);
+//   const [showErrorModal, setShowErrorModal] = useState<boolean>(false);
+//   const [showEditModal, setShowEditModal] = useState<boolean>(false);
+//   const [showLoginModal, setShowLoginModal] = useState<boolean>(false);
+//   const [editingCommentId, setEditingCommentId] = useState<number | null>(null);
+
+//   //게시글 세부 정보 로드
+//   useEffect(() => {
+//     const loadPostDetails = async () => {
+//       if (id) {
+//         try {
+//           const postDetail = await fetchPostDetail(id);
+//           setPost(postDetail);
+//           // const comments = await fetchComments(id);
+//           setComments(postDetail.comments);
+//           setCommentCount(postDetail.comments.length);
+//           setLikes(postDetail.likes || 0);
+//           setIsLiked(postDetail.is_liked);
+//         } catch (error) {
+//           console.error('게시글 로드 오류 발생', error);
+//         }
+//       }
+//     };
+//     loadPostDetails();
+//   }, []); // 빈 배열로 설정
+
+//   const handleCommentChange = (e: React.ChangeEvent<HTMLTextAreaElement>) => {
+//     setNewComment(e.target.value);
+//   };
+
+//   // 댓글 처리 로직
+//   const handleCommentSubmit = async () => {
+//     if (!user) {
+//       // 비로그인 일시 등록 버튼 클릭시 로그인페이지 이동
+//       setShowLoginModal(true);
+//       setTimeout(() => {
+//         setShowLoginModal(false);
+//         navigate('/login');
+//       }, 2000);
+//       return;
+//     }
+//     if (!(user && post && newComment.trim().length > 0)) {
+//       setShowErrorModal(true);
+//       setTimeout(() => setShowErrorModal(false), 2000);
+//       return;
+//     } else {
+//       setShowErrorModal(false);
+//     }
+
+//     // 댓글 수정 처리
+//     const handleCommentEdit = async () => {
+//       if (!editingCommentId) throw new Error('Invalid editingCommentId');
+
+//       const updatedComment = `${newComment} (수정됨)`;
+
+//       try {
+//         await updateComment(editingCommentId, updatedComment);
+
+//         setComments(
+//           comments.map((comment) =>
+//             comment.id === editingCommentId
+//               ? { ...comment, content: updatedComment }
+//               : comment
+//           )
+//         );
+//         setEditingCommentId(null);
+//         setShowEditModal(true);
+//         setTimeout(() => setShowEditModal(false), 2000);
+//       } catch (error) {
+//         console.error('댓글 수정 에러 발생', error);
+//       }
+//     };
+
+//     // 댓글 생성 처리
+//     const handleCommentCreate = async () => {
+//       const res = await createComment(post.id, newComment);
+//       const newCommentEntry: Comment = {
+//         id: res.id,
+//         content: res.content,
+//         created_at: res.created_at,
+//         updated_at: res.updated_at,
+//         is_deleted: res.deleted,
+//         author: {
+//           id: user.id,
+//           role: user.role,
+//           workshop_name: user.workshop_name,
+//         },
+//       };
+//       setComments(() => [newCommentEntry, ...comments]);
+//       setShowModal(true);
+//       setTimeout(() => setShowModal(false), 2000);
+//     };
+
+//     try {
+//       if (editingCommentId) {
+//         await handleCommentEdit();
+//       } else {
+//         await handleCommentCreate();
+//       }
+//       setNewComment('');
+//     } catch (error) {
+//       console.error('댓글 등록 에러 발생', error); // 체크용
+//     }
+//   };
+
+//   // 댓글 삭제 처리
+//   const handleCommentDelete = async (id: number) => {
+//     await deleteComment(id);
+//     const updatedComments = comments.filter((comment) => comment.id !== id);
+//     setComments(updatedComments);
+
+//     setCommentCount(updatedComments.length);
+//     setShowDeleteModal(true);
+//     setTimeout(() => setShowDeleteModal(false), 2000);
+//   };
+
+//   // 댓글 수정 모드 활성화
+//   const handleEditComment = (id: number) => {
+//     const commentToEdit = comments.find((comment) => comment.id === id);
+//     if (commentToEdit) {
+//       setNewComment(commentToEdit.content.replace(' (수정됨)', ''));
+//       setEditingCommentId(id);
+//     }
+//   };
+
+//   // 좋아요 토글 처리
+//   const handleLike = async () => {
+//     if (!user || !post) return;
+//     const newLikedStatus = !isLiked;
+//     setIsLiked(newLikedStatus);
+//     setLikes((prevLikes) => prevLikes + (newLikedStatus ? 1 : -1));
+
+//     try {
+//       await toggleLikePost(newLikedStatus, post.id);
+//     } catch (error) {
+//       console.error('좋아요 상태 업데이트 오류 발생', error);
+//       setIsLiked(!newLikedStatus); // 오류시 되돌리기
+//       setLikes((prevLikes) => prevLikes - (newLikedStatus ? 1 : -1)); // 원래 상태로 되돌리기
+//     }
+//   };
+
+//   const handleEditButtonClick = () => {
+//     if (id) {
+//       navigate(`/communitypost/${id}`);
+//     }
+//   };
+
+//   const closeDeleteModal = () => {
+//     setShowConfirmModal(false);
+//   };
+
+//   const handleDeletePost = () => {
+//     setShowConfirmModal(true);
+//   };
+
+//   const confirmDeletePost = async () => {
+//     if (post) {
+//       await deletePost(post.id);
+//       navigate(`/community`);
+//     }
+//     setShowConfirmModal(false);
+//   };
+
+//   const handleBackButtonClick = () => {
+//     navigate('/community');
+//   };
+
+//   return (
+//     <div className="flex justify-center items-center px-4">
+//       <div className="w-full max-w-[81.25rem] bg-white p-6 shadow-lg rounded-lg mt-[6.25rem] mb-[6.25rem]">
+//         <button onClick={handleBackButtonClick} className="mb-4 text-gray-400">
+//           뒤로가기
+//         </button>
+//         {post && (
+//           <>
+//             <div className="mb-4 flex items-center justify-between">
+//               <div className="flex items-center">
+//                 <h1 className="text-black text-2xl font-medium">
+//                   [{post.category}]
+//                 </h1>
+//                 <h1 className="text-xl font-bold ml-2">{post.title}</h1>
+//                 <span className="text-gray-500 text-lg ml-2 flex items-center">
+//                   <FaCommentDots className="mr-1" />({commentCount})
+//                 </span>
+//               </div>
+//               {user && post.author.id === user.id && (
+//                 <div className="flex space-x-4">
+//                   <button
+//                     onClick={handleEditButtonClick}
+//                     className="text-gray-400 hover:text-blue-600 transition duration-300 text-sm"
+//                   >
+//                     수정
+//                   </button>
+//                   <button
+//                     onClick={handleDeletePost}
+//                     className="text-gray-400 hover:text-red-600 transition duration-300 text-sm"
+//                   >
+//                     삭제
+//                   </button>
+//                 </div>
+//               )}
+//             </div>
+//             <div className="text-gray-400 mb-10">
+//               <span className="font-bold mr-2">{post.author.nickname} </span>
+//               <span className="text-[#F26749] mr-2">
+//                 {post.author.workshop_name || post.author.company_name}
+//               </span>
+//               <span className="text-sm">
+//                 {new Date(post.created_at).toLocaleString()}
+//               </span>
+//             </div>
+//             {post.images && post.images.length > 0 && (
+//               <div className="flex flex-wrap mb-6">
+//                 {post.images.map((image) => (
+//                   <img
+//                     key={image.id}
+//                     src={image.image_url}
+//                     alt={`게시글 이미지 ${image.id}`}
+//                     className="w-80 h-80 mb-4"
+//                   />
+//                 ))}
+//               </div>
+//             )}
+//             <div className="mb-6 w-full">
+//               <p className="mt-2 text-base leading-relaxed text-gray-700 break-words">
+//                 {post.content}
+//               </p>
+//             </div>
+//             <div className="flex justify-center mt-10 mb-10">
+//               <button
+//                 onClick={handleLike}
+//                 className={`border rounded-lg px-4 py-1 flex items-center transition duration-300 ${isLiked ? 'bg-[#F26749] text-white border-[#F26749] font-bold' : 'border-[#F26749] text-[#F26749] font-bold'}`}
+//               >
+//                 <BsChatHeart className="mr-1 h-5 w-5" />
+//                 좋아요 ! {likes}
+//               </button>
+//             </div>
+//             <CommentInput
+//               newComment={newComment}
+//               onCommentChange={handleCommentChange}
+//               onCommentSubmit={handleCommentSubmit}
+//             />
+//             <CommentList
+//               comments={comments}
+//               currentUserId={user ? user.id : 0}
+//               onDelete={handleCommentDelete}
+//               onEdit={handleEditComment}
+//             />
+
+//             {showModal && (
+//               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+//                 <div className="bg-white p-6 rounded-lg shadow-lg">
+//                   <h2 className="text-lg font-semibold text-center">
+//                     댓글이 등록 되었습니다 !
+//                   </h2>
+//                 </div>
+//               </div>
+//             )}
+//             {showConfirmModal && (
+//               <ConfirmModal
+//                 handleDelete={confirmDeletePost}
+//                 closeDeleteModal={closeDeleteModal}
+//               />
+//             )}
+//             {showDeleteModal && (
+//               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+//                 <div className="bg-white p-6 rounded-lg shadow-lg">
+//                   <h2 className="ext-lg font-semibold text-center">
+//                     댓글이 삭제 되었습니다 !
+//                   </h2>
+//                 </div>
+//               </div>
+//             )}
+//             {showErrorModal && (
+//               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+//                 <div className="bg-white p-6 rounded-lg shadow-lg">
+//                   <h2 className="ext-lg font-semibold text-center">
+//                     최소 1글자 이상 입력하세요 !
+//                   </h2>
+//                 </div>
+//               </div>
+//             )}
+//             {showEditModal && (
+//               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+//                 <div className="bg-white p-6 rounded-lg shadow-lg">
+//                   <h2 className="ext-lg font-semibold text-center">
+//                     댓글이 수정 되었습니다 !
+//                   </h2>
+//                 </div>
+//               </div>
+//             )}
+//             {showLoginModal && (
+//               <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50">
+//                 <div className="bg-white p-6 rounded-lg shadow-lg">
+//                   <h2 className="ext-lg font-semibold text-center">
+//                     로그인 후 댓글을 등록 할 수 있습니다 !
+//                   </h2>
+//                 </div>
+//               </div>
+//             )}
+//           </>
+//         )}
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default CommunityDetail;
--- src/pages/community/CommunityDetailAPI.ts
+++ src/pages/community/CommunityDetailAPI.ts
@@ -65,3 +65,81 @@ export const createPost = async (data: FormData) => {
   });
   return response.data;
 };
+
+// 댓글 가져오기
+// export const fetchComments = async (postId: number | string) => {
+//   const response = await axiosInstance.get(`/api/comment/comments/`, {
+//     headers: {
+//       'Cache-Control': 'no-cache',
+//     },
+//   });
+//   return response.data;
+// };
+
+// import axiosAuthInstance from '../../api/axiosAuthInstance';
+// import axiosInstance from '../../api/axiosInstance';
+
+// // 게시글 작성 함수
+// export const fetchPostDetail = async (post_id: number | string) => {
+//   const response = await axiosInstance.get(`/api/posts/${post_id}/`);
+//   return response.data;
+// };
+
+// // 게시글 삭제 함수
+// export const deletePost = async (post_id: number) => {
+//   await axiosAuthInstance.delete(`/api/posts/${post_id}/delete/`);
+// };
+
+// // 게시글 수정 함수
+// export const updatePost = async (
+//   post_id: number,
+//   updatedContent: { title: string; content: string }
+// ) => {
+//   const response = await axiosAuthInstance.patch(
+//     `/api/posts/${post_id}/update/`,
+//     { updatedContent }
+//   );
+//   return response.data;
+// };
+
+// // 댓글 작성 함수
+// export const createComment = async (post_id: number, content: string) => {
+//   const response = await axiosAuthInstance.post(
+//     `/api/comment/${post_id}/comments/create/`,
+//     { content }
+//   );
+//   return response.data;
+// };
+
+// // 댓글 수정 함수
+// export const updateComment = async (
+//   comment_id: number, // 수정할 댓글 ID
+//   content: string
+// ) => {
+//   const response = await axiosAuthInstance.patch(
+//     `/api/comment/${comment_id}/update/`,
+//     { content }
+//   );
+//   return response.data;
+// };
+
+// // 댓글 삭제 API 함수
+// export const deleteComment = async (comment_id: number) => {
+//   await axiosAuthInstance.delete(`/api/comment/${comment_id}/delete/`);
+// };
+
+// // 게시글 좋아요 토글 함수
+// export const toggleLikePost = async (is_liked: boolean, post_id: number) => {
+//   const response = await axiosAuthInstance.post(`/api/posts/${post_id}/like/`, {
+//     is_liked,
+//   });
+//   return response.data;
+// };
+
+// // 게시글 작성 이미지 렌더링 확인용
+// export const createPost = async (data: FormData) => {
+//   const response = await axiosAuthInstance.post('/api/posts/create/', data, {
+//     headers: { 'Content-Type': 'multipart/form-data' },
+//   });
+//   return response.data;
+// };
